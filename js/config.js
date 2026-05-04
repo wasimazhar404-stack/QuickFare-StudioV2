160b94915c5865878fb478289dd548dd3a04bf5e
wasimazhar404-stack <wasimazhar404@gmail.com>
2026-05-04 13:00:38 +0500

    Initial upload of js/config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    PEXELS_KEY: '', // To be filled by user
+    QWEN_KEY: '',   // To be filled by user
+    FALLBACK_BGS: [
+        'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    ],
+    CANVAS_WIDTH: 1080,
+    CANVAS_HEIGHT: 1350,
+    DEFAULT_FONTS: ['Anton', 'Montserrat', 'Oswald', 'Playfair Display']
+};
+
+window.CONFIG = CONFIG;
